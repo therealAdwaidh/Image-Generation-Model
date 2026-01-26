3ee58b26f959eabf7d444b63f67f35bb26015d7b
Adwaidh <adwaidhcke2@gmail.com>
2026-01-26 19:04:00 +0530

    fourth commit

--- client/src/components/ResultCard.jsx
+++ client/src/components/ResultCard.jsx
@@ -36,7 +36,7 @@ export default function ResultCard({ prompt }) {
     }, 500);
 
     try {
-      const response = await fetch('http://localhost:5000/api/image', {
+      const response = await fetch('/api/image', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
